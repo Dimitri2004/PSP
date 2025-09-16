49766aae8b23b81b3493b53ba960fe33a3102aae
dam <daapariciopereira2@danielcastelao.org>
2025-09-16 10:25:19 +0200

    Ejercicios de Tarea3 crear archivo de texto con comandos y puesta en terminal de usuario creando nuevo archivo 0.1

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 
 public class Tarea3 {
     public static void main(String[] arg) throws IOException {
-        Scanner sc=new Scanner(System.in);
-        ProcessBuilder pb=new ProcessBuilder("gnome-text-editor");
-        File log=new File("log");
-        pb.redirectErrorStream(true);
-        pb.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
-        Process p= pb.start();
-        assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
-        assert pb.redirectOutput().file() == log;
-        if (p.getInputStream().read() != -1) throw new AssertionError();
-
-        System.out.println("Quieres crear nuevo archivo:Si/No");
+        Scanner sc=new Scanner(System.in);        System.out.println("Quieres crear nuevo archivo:Si/No");
         String r=sc.next();
 
         if (Objects.equals(r, "Si")){
             Editor();
         }
     }
-    public static void Editor(){
+    public static void Editor() throws IOException {
         Scanner sc=new Scanner(System.in);
         System.out.println("Cual es el nombre del nuevo archivo :");
         String name=sc.next();
+        ProcessBuilder pb=new ProcessBuilder("gnome-text-editor");
+        File log=new File(name);
+        pb.redirectErrorStream(true);
+        Process p= pb.start();
+        assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
+        assert Objects.equals(pb.redirectOutput().file(), log);
+        if (p.getInputStream().read() != -1) throw new AssertionError();
     }
 
 
