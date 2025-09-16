769f7f34317fabaf4387c17dd90cfb39032ad3d7
dam <daapariciopereira2@danielcastelao.org>
2025-09-16 10:19:46 +0200

    Ejercicios de Tarea3 crear archivo de texto con comandos y puesta en terminal de usuario

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Tarea3 {
     public static void main(String[] arg) throws IOException {
+        Scanner sc=new Scanner(System.in);
         ProcessBuilder pb=new ProcessBuilder("gnome-text-editor");
         File log=new File("log");
         pb.redirectErrorStream(true);
@@ -11,5 +14,19 @@ public class Tarea3 {
         assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
         assert pb.redirectOutput().file() == log;
         if (p.getInputStream().read() != -1) throw new AssertionError();
+
+        System.out.println("Quieres crear nuevo archivo:Si/No");
+        String r=sc.next();
+
+        if (Objects.equals(r, "Si")){
+            Editor();
+        }
     }
+    public static void Editor(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Cual es el nombre del nuevo archivo :");
+        String name=sc.next();
+    }
+
+
 }
