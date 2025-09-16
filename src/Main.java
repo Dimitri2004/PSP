dbcf429d87a299a12e438b81316a33e6ad8b10f6
dam <daapariciopereira2@danielcastelao.org>
2025-09-16 09:32:22 +0200

    Ejercicios de Tarea2 Calculo de memoria en MiB

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
+    Runtime r= Runtime.getRuntime();
 
+        System.out.println("Espacio libre :"+r.freeMemory()/1024);
+        System.out.println("Espacio total :"+r.totalMemory()/1024);
+        System.out.println("Espacio máximo :"+r.maxMemory()/1024);
     }
 }
