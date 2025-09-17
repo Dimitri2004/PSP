57647f19fdf3b6b71194eebbcc8885e04924325a
dam <daapariciopereira2@danielcastelao.org>
2025-09-17 09:57:44 +0200

    Ejercicios de Tarea4 Muestra ruta de direcctorio de trabajo 0.2

--- src/Tarea4.java
+++ src/Tarea4.java
@@ -1,4 +1,5 @@
 import java.io.File;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -6,10 +7,19 @@ public class Tarea4 {
     public static void main(String[] args) throws IOException {
         String cmdo="dir";
         String ruta="user.dir";
-        ProcessBuilder pb=new ProcessBuilder(cmdo,ruta);
+        String dir="/home/dam/Escritorio/PSP/Tarea2";
+        String dir2="/home/dam/Escritorio/PSP/Tarea1";
+        ProcessBuilder pb=new ProcessBuilder(cmdo);
         Properties properties = System.getProperties();
+        pb.inheritIO();
 
         System.out.println( properties.getProperty(ruta));
+
+        properties.setProperty(ruta,dir2);
+        System.out.println( properties.getProperty(ruta));
+
+
+
         pb.start();
 
     }
