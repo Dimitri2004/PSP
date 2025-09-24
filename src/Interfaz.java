ce18f06db18e5580f81788585bacf90c66125583
dam <daapariciopereira2@danielcastelao.org>
2025-09-24 10:03:44 +0200

    Ejercicios de Tarea6 comentada

---
+++ src/Interfaz.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Interfaz {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String opcion;
+        while(true) {
+            System.out.println("Introfuce host/ip o ('salir' para terminar)");
+            opcion = sc.next().toLowerCase();//PARA LISTILLOS
+            if (opcion.equals("salir")) {
+                break;//Interrumpe programa si se escribe salir
+            }
+            Lanzador.Lanzar(opcion);//Envia el dato hacia donde va a ejecutar el proceso
+        }
+    }
+}
---
+++ src/Lanzador.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Lanzador {
+    public static void Lanzar(String opcion){
+        String comando="ping";
+        String distancia="-c";
+        String cantidad="4";
+        InputStream consola;
+        String estado;
+                try {
+                    ProcessBuilder pb = new ProcessBuilder(comando, distancia, cantidad, opcion);//Creamos el builder con la opcion de host o ip enviada desde interfaz
+                    Process pv = pb.start();
+                    int f=pv.waitFor();//Codigo de salida 1 o 0
+                    if (f==0){
+                        consola=pv.getInputStream();//Realiza una asignacion de datos del processBuiter inicializado anteriormente
+                        estado="[OK]";
+
+                    }else {
+                        consola=pv.getErrorStream();//Asigna un estado de fallo en caso de que una linea sea incorrecta
+                        estado="[Fallo]";
+                    }
+                    BufferedReader lectura = new BufferedReader(new InputStreamReader(consola));//Realiza la lectura de los ping de consola al que le asignamos el estado ok y los reproduce añadiendo el mismo
+
+                    while (true) {
+                        String linea = lectura.readLine();
+                        if (linea==null){
+                            break;//En caso de que termine pues pare el proceso
+                        }
+                        System.out.println(estado+linea);
+                    }
+                    System.out.println("Operacion terminada "+f);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);//Error de e/s
+                } catch (Throwable e) {
+                    throw new RuntimeException("No funciona" + e.getMessage());//Error en ejecuccion
+                }
+        }
+    }
