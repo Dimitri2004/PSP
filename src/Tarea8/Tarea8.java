d1aa88570d48dbbad7a2a3423345f4995f497d63
dam <daapariciopereira2@danielcastelao.org>
2025-09-30 10:06:32 +0200

    Ejercicios de Tarea8 y Tarea9

---
+++ src/Tarea8/Tarea8.java
@@ -0,0 +1,33 @@
+package Tarea8;
+
+public class Tarea8 extends Thread {
+    //Nombre y valor de paciancia
+    private int paciencia=4;
+    public Tarea8(String nombre,int paciencia){
+        super(nombre);
+        this.paciencia=paciencia;
+    }
+    @Override
+    public void run() {
+        int i;
+        for (i = 0; i <= paciencia; i++) {
+            System.out.println("["+getName()+"] Nivel de enfado:"+i);
+            if (i == 4) {
+                System.out.println("[" +getName() + "] Paciencia agotada:" + i);
+            }
+        }
+    }
+
+    //Empezamos las tareas con start() y llamamos run()
+    public static class Thread {
+        public static void main(String[] args) {
+            System.out.println("empezando");
+            new Tarea8("Diego",4).start();
+            new Tarea8("Iago",4).start();
+            new Tarea8("Manuel",4).start();
+            new Tarea8("Damian",4).start();
+
+        }
+
+    }
+}
---
+++ src/Tarea8/Tarea8_2.java
@@ -0,0 +1,33 @@
+package Tarea8;
+
+public class Tarea8_2 extends Thread {
+
+
+    private int num;
+    public Tarea8_2(int num){
+        this.num=num;
+    }
+    @Override
+    public void run(){
+        int a=0;
+        int b=1;
+        if (num<=0){
+            System.out.println(a);//Saca 0
+        }if (num>=1){
+            System.out.println(a);//Saca 1
+        }if (num>=2){
+            System.out.println(b);//Saca 1
+        }
+        for (int i=2;i<=num;i++){//operacion fibonaci
+           int resultado=a+b;// 0+1 num veces ->1+1->1+2
+            System.out.println(resultado);//1
+            a=b;//2,3,5,8
+            b=resultado;//igualamos
+        }
+
+    }
+
+    public static void main(String[] args){
+        new Tarea8_2(50).start();
+    }
+}
