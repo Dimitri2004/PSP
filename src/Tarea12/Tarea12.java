c7dd3ca3e1572cdfd39cb18bda28902a72105b42
dam <daapariciopereira2@danielcastelao.org>
2025-10-07 10:46:08 +0200

    Tarea12 sacando 3 hilos de ejecucion continua comentada

--- src/Tarea12/Tarea12.java
+++ src/Tarea12/Tarea12.java
@@ -6,33 +6,33 @@ public class Tarea12 extends Thread {
     private int suma;
     private String nombre;
 
-    public Tarea12(int nume) {
-        this.nume = nume;
+    public Tarea12(int nume) {// 0 par, 1 impar, 2 terminado en 3 o 4
+        this.nume = nume;// tipo de suma
     }
 
     @Override
-    public void run() {
-        switch (nume) {
-            case 0:
+    public void run() { //realiza la suma segun el tipo
+        switch (nume) {// 0 par, 1 impar, 2 terminado en 3 o 4
+            case 0:// par
                 nombre="Par";
                 for (int i = 0; i < 1923; i++) {
-                    if (i % 2 == 0) {
+                    if (i % 2 == 0) { // Si el número es par
                         suma += i;
                     }
                 }
                 break;
-            case 1:
+            case 1: // impar
                 nombre ="Impar";
                 for (int i = 0; i < 1923; i++) {
-                    if (i % 2 != 0) {
+                    if (i % 2 != 0) { // Si el número es impar
                         suma += i;
                     }
                 }
                 break;
-            case 2:
+            case 2: // terminado en 3 o 4
                 nombre="Terminado 3 o 4";
                 for (int i = 0; i < 1923; i++) {
-                    int ultimoDigito = i % 10;
+                    int ultimoDigito = i % 10; // Obtener el último dígito
                     if (ultimoDigito == 3 || ultimoDigito == 4) {
                         suma += i;
                     }
@@ -41,25 +41,25 @@ public class Tarea12 extends Thread {
         }
     }
 
-    private int getSuma() {
+    private int getSuma() { // devuelve la suma realizada
         return suma;
     }
 
-    private String getNombre(){
+    private String getNombre(){ // devuelve el nombre del tipo de suma
         return nombre;
     }
     public static void main(String[] args)  {
-        Tarea12[] hilos = new Tarea12[3];
-        for (int i = 0; i < hilos.length; i++) {
-            hilos[i] = new Tarea12(i);
-            hilos[i].start();
+        Tarea12[] hilos = new Tarea12[3]; // Creamos un array de hilos
+        for (int i = 0; i < hilos.length; i++) { // Creamos e iniciamos los hilos
+            hilos[i] = new Tarea12(i); // 0 par, 1 impar, 2 terminado en 3 o 4
+            hilos[i].start(); // Iniciamos el hilo
         }
-        try {
+        try { // Esperamos a que terminen los hilos y mostramos el resultado
             for (Tarea12 hilo : hilos) {
-                hilo.join();
-                System.out.println("Resultado del hilo " + hilo.getNombre() + " " + hilo.getSuma());
+                hilo.join(); // Esperamos a que termine el hilo
+                System.out.println("Resultado del hilo " + hilo.getNombre() + " " + hilo.getSuma()); // Mostramos el resultado
             }
-        }catch (InterruptedException e){
+        }catch (InterruptedException e){ // Capturamos la excepción
             System.out.println("Error ejecucion hilo "+e);
         }
     }
